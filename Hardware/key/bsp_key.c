ca4edd62bb0f769320399cfdec37c95925d01bf8
Ruifu Su <ryan@ruifutech.net>
2022-11-06 12:20:03 +0800

    完成外部中断控制走马灯实验

--- Hardware/key/bsp_key.c
+++ Hardware/key/bsp_key.c
@@ -1,9 +1,11 @@
 #include "bsp_key.h"
-
 #include "bsp_led.h"
 #include "gd32f4xx.h"
+#include "gd32f4xx_exti.h"
 #include "gd32f4xx_gpio.h"
+#include "gd32f4xx_misc.h"
 #include "gd32f4xx_rcu.h"
+#include "gd32f4xx_syscfg.h"
 #include "stdint.h"
 #include "stdio.h"
 #include "systick.h"
@@ -11,8 +13,19 @@
 void key_gpio_config(void) {
   /* 开启时钟 */
   rcu_periph_clock_enable(RCU_GPIOA);
+  rcu_periph_clock_enable(RCU_SYSCFG);
   /* 配置GPIO模式 */
   gpio_mode_set(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN, GPIO_PIN_0);
+  /* 设置中断优先级 */
+  nvic_irq_enable(EXTI0_IRQn, 3, 3);
+  /* 连接中断线 */
+  syscfg_exti_line_config(EXTI_SOURCE_GPIOA, EXTI_SOURCE_PIN0);
+  /* 初始化中断 */
+  exti_init(EXTI_0, EXTI_INTERRUPT, EXTI_TRIG_BOTH);
+  /* 使用中断 */
+  exti_interrupt_enable(EXTI_0);
+  /* 清除中断标志位 */
+  exti_interrupt_flag_clear(EXTI_0);
 }
 
 uint8_t led_status = 0x00;
@@ -24,36 +37,52 @@ void key_scan(void) {
     if (SET == gpio_input_bit_get(GPIOA, GPIO_PIN_0)) {
       /* 执行对应的功能 */
       printf("Key press!\n");
-        switch (led_status) {
-        case 0x00:
-          LED1 = 1;
-          led_status = 0x01;
-          break;
-        case 0x01:
-          LED2 = 1;
-          led_status = 0x02;
-          break;
-        case 0x02:
-          LED3 = 1;
-          led_status = 0x03;
-          break;
-        case 0x03:
-          LED4 = 1;
-          led_status = 0x04;
-          break;
-        case 0x04:
-          LED1 = 0;
-          LED2 = 0;
-          LED3 = 0;
-          LED4 = 0;
-          led_status = 0x00;
-          break;
-        }
+      switch (led_status) {
+      case 0x00:
+        LED1 = 1;
+        led_status = 0x01;
+        break;
+      case 0x01:
+        LED2 = 1;
+        led_status = 0x02;
+        break;
+      case 0x02:
+        LED3 = 1;
+        led_status = 0x03;
+        break;
+      case 0x03:
+        LED4 = 1;
+        led_status = 0x04;
+        break;
+      case 0x04:
+        LED1 = 0;
+        LED2 = 0;
+        LED3 = 0;
+        LED4 = 0;
+        led_status = 0x00;
+        break;
+      }
 
       /* 检测按键松开 */
       while (gpio_input_bit_get(GPIOA, GPIO_PIN_0) == SET)
         ;
       printf("Key release!\n");
     }
+  }
+}
+
+extern uint8_t riding_led_status;
+void EXTI0_IRQHandler(void) {
+  if (exti_interrupt_flag_get(EXTI_0) == SET) {
+    if (gpio_input_bit_get(GPIOA, GPIO_PIN_0) == SET) {
+      riding_led_status = !riding_led_status;
+      if (!riding_led_status) {
+        printf("Stop LED\n");
+      }
+      printf("-------Key pressed------\n");
+    } else {
+      printf("-------Key released------\n");
+    }
+    exti_interrupt_flag_clear(EXTI_0);
   }
 }
--- User/main.c
+++ User/main.c
@@ -1,23 +1,55 @@
 #include "main.h"
+#include "bsp_key.h"
 #include "bsp_led.h"
 #include "bsp_usart.h"
 #include "gd32f4xx.h"
+#include "gd32f4xx_misc.h"
 #include "stdint.h"
 #include "systick.h"
 #include <stdio.h>
-#include "bsp_key.h"
+
+uint8_t riding_led_status = 1;
+uint8_t led_on = 0x01;
+
+void set_led(uint8_t led_on) {
+  LED1 = led_on == 0x01;
+  LED2 = led_on == 0x02;
+  LED3 = led_on == 0x03;
+  LED4 = led_on == 0x04;
+}
 
 int main(void) {
+  /* NVIC 优先级分组 */
+  nvic_priority_group_set(NVIC_PRIGROUP_PRE2_SUB2);
+
   systick_config();
   led_gpio_config();
   usart_gpio_config(9600);
-
+  key_gpio_config();
   //   usart_send_string("Hello world\n");
   //   printf("%.4f\n", 3 / 2.0);
 
   // char customer_number[] = {"1561362A"};
   // printf("The Customer Number of Ruifu Su is %s", customer_number);
   while (1) {
-    key_scan();
+    // key_scan();
+    delay_1ms(500);
+    if (riding_led_status) {
+      set_led(led_on);
+      switch (led_on) {
+      case 0x01:
+        led_on = 0x02;
+        break;
+      case 0x02:
+        led_on = 0x03;
+        break;
+      case 0x03:
+        led_on = 0x04;
+        break;
+      case 0x04:
+        led_on = 0x01;
+        break;
+      }
+    }
   }
 }
